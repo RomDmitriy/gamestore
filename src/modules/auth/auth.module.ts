4e4a6f04df4b4f9466a67e8b9a4e61d06110ed47
RomDmitriy <aisek72@gmail.com>
2023-05-07 13:56:07 +0300

    Фикс генератора jwt

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,4 +1,20 @@
 import { Module } from '@nestjs/common';
+import { AuthenticationController } from './authentication/authentication.controller';
+import { AuthenticationService } from './authentication/authentication.service';
+import { JwtService } from '@nestjs/jwt';
+import { JwtGeneratorService } from './jwt/jwtGenerator.service';
+import { PrismaService } from 'src/prisma.service';
+import { ConfigService } from '@nestjs/config';
 
-@Module({})
+@Module({
+  controllers: [AuthenticationController],
+  providers: [
+    AuthenticationService,
+    JwtGeneratorService,
+    PrismaService,
+    ConfigService,
+    JwtService,
+  ],
+  exports: [JwtGeneratorService],
+})
 export class AuthModule {}
