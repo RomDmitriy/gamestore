0c38a4cdff4ab70e0e96fccded69e5e0fdc774b0
RomDmitriy <aisek72@gmail.com>
2023-05-07 12:48:33 +0300

    Добавлен JWT генератор

---
+++ src/dto/JWT_info.dto.ts
@@ -0,0 +1,4 @@
+// по желанию можно добавить больше информации в токен
+export interface jwtInfo {
+  id: string;
+}
---
+++ src/modules/auth/jwt/jwt.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { jwtInfo } from 'src/dto/JWT_info.dto';
+
+const enum TOKEN_TYPES {
+  'access' = 'access',
+  'refresh' = 'refresh',
+}
+
+@Injectable()
+export class JwtGeneratorService {
+  constructor(
+    private readonly jwt: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  generateAccessToken(userInfo: jwtInfo): string {
+    return this.jwt.sign(
+      { userInfo, type: TOKEN_TYPES.access },
+      {
+        privateKey: this.configService.getOrThrow('JWT_ACCESS_KEY'),
+        expiresIn: 900,
+      },
+    );
+  }
+
+  generateRefreshToken(userInfo: jwtInfo): string {
+    return this.jwt.sign(
+      { userInfo, type: TOKEN_TYPES.refresh },
+      {
+        privateKey: this.configService.getOrThrow('JWT_REFRESH_KEY'),
+        expiresIn: '30d',
+      },
+    );
+  }
+}
