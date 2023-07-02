a3b0f152a8afa5e2e2d5039a2f03bfb69f628781
RomDmitriy <aisek72@gmail.com>
2023-07-02 13:02:00 +0300

    Защита от падения API при недоступности БД

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -4,7 +4,14 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
-    await this.$connect();
+    try {
+      await this.$connect();
+    } catch (err) {
+      // если не получилось подключиться,
+      // но чтобы приложение не падало
+      // P.S. по сути, из рабочего тогда остаётся лишь документация
+      console.log(err);
+    }
   }
 
   async enableShutdownHooks(app: INestApplication) {
